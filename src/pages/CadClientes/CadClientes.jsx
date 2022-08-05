a0cb7be5d72dda769ed6f4c63b8b9158516d26aa
Magali Lima <99837700+MagaliLimaDev@users.noreply.github.com>
2022-08-05 20:24:24 -0300

    ajustando código
    ajustando código

--- src/pages/CadClientes/CadClientes.jsx
+++ src/pages/CadClientes/CadClientes.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import S from "./CadClientes.css";
+import "./CadClientes.css";
 import { cpf } from 'cpf-cnpj-validator'; 
 
 const CadClientes = () => {
@@ -18,7 +18,7 @@ const CadClientes = () => {
 
   function validaEmail(){
     if(inputEmail.indexOf('@') > 2 && inputEmail.indexOf('.com') > inputEmail.indexOf('@')){
-      alert("Email Válido")
+      //alert("Email Válido")
     }else{
       alert("Email Inválido")
     }
@@ -33,24 +33,25 @@ const CadClientes = () => {
   }
 
   return (
-    <section className={S.cadastro}>
-      <div className={S.text}>
+    <section className='cadastro'>
+      <div className='text'>
         <h3>Faça seu cadastro aqui</h3>
         <p>oioioi quero cadastro,</p>
         <br></br>
         <p>TESTE TESTE</p>
       </div>
 
-      <form className={S.formCadastro}>
+      <form className='formCadastro'>
         <label>Nome:</label>
         <input type="text" />
-        <label>E-mail:</label>
         <label>Telefone:</label>
         <input type="text" />
+
+        <label>E-mail:</label>
         <input type="email" value={inputEmail} onChange={({target})=>handleInputEmail(target)}/>
         <label>CPF:</label>
         <input type="text" value={inputCPF} onChange={({target})=>handleInputCPF(target)}/>
-        <div className={S.radioInput}>
+        <div className='radioInput'>
           <input type="radio" name='genero' value="masculino"/>
           <label htmlFor="Masculino">Masculino</label>
           <input type="radio" name='genero' value="feminino"/>
