70499cda037b622e1defe780281f07482d023f59
Magali Lima <99837700+MagaliLimaDev@users.noreply.github.com>
2022-08-05 22:04:19 -0300

    ajustando codigo
    ajustando codigo

--- src/pages/CadClientes/CadClientes.jsx
+++ src/pages/CadClientes/CadClientes.jsx
@@ -5,15 +5,33 @@ import { cpf } from 'cpf-cnpj-validator';
 const CadClientes = () => {
   const [inputEmail, setInputEmail] = useState('')
   const [inputCPF, setInputCPF] = useState('')
+  const [inputNome, setInputNome] = useState('')
+  const [inputTel, setInputTel] = useState('')
+  const [inputGenero, setInputGenero] = useState('')
 
   function handleInputEmail(target){
     setInputEmail(target.value)
-    console.log(inputEmail)
+    //console.log(inputEmail)
   }
   
   function handleInputCPF(target){
     setInputCPF(target.value)
-    console.log(inputCPF)
+    //console.log(inputCPF)
+  }
+
+  function handleInputNome(target){
+    setInputNome(target.value)
+    //console.log(inputEmail)
+  }
+  
+  function handleInputTel(target){
+    setInputTel(target.value)
+    //console.log(inputCPF)
+  }
+
+  function handleInputGenero(target){
+      setInputGenero(target.value);
+    //console.log(inputEmail)
   }
 
   function validaEmail(){
@@ -32,6 +50,26 @@ const CadClientes = () => {
     }
   }
 
+  function cadastrar() {
+    console.log("EMail: " + inputEmail);
+    console.log("cpf: " + inputCPF);
+    console.log("Nome: " + inputNome);
+    console.log("Telefone: " + inputTel);
+    console.log("Genero: " + inputGenero);
+    
+
+    limpaForm();
+    
+}
+
+function limpaForm() {
+  setInputEmail("");
+  setInputCPF("");
+  setInputNome("");
+  setInputTel("");
+  setInputGenero(false);
+}
+
   return (
     <section className='cadastro'>
       <div className='text'>
@@ -43,25 +81,29 @@ const CadClientes = () => {
 
       <form className='formCadastro'>
         <label>Nome:</label>
-        <input type="text" />
+        <input type="text" value={inputNome} onChange={({target})=>handleInputNome(target)} />
         <label>Telefone:</label>
-        <input type="text" />
+        <input type="text" value={inputTel} onChange={({target})=>handleInputTel(target)}/>
 
         <label>E-mail:</label>
         <input type="email" value={inputEmail} onChange={({target})=>handleInputEmail(target)}/>
         <label>CPF:</label>
         <input type="text" value={inputCPF} onChange={({target})=>handleInputCPF(target)}/>
         <div className='radioInput'>
-          <input type="radio" name='genero' value="masculino"/>
+          <input type="radio" name='genero'  value="masculino" onChange={({target})=>handleInputGenero(target)}/>
           <label htmlFor="Masculino">Masculino</label>
-          <input type="radio" name='genero' value="feminino"/>
+          <input type="radio" name='genero'  value="feminino" onChange={({target})=>handleInputGenero(target)}/>
           <label htmlFor="feminino">Feminino</label>
         </div>
         <button onClick={(event)=>{
           event.preventDefault()
-          validaEmail()
-          validaCPF()
-          }}>Enviar cadastro</button>
+          validaEmail();
+          validaCPF();
+          cadastrar();
+          }}>Enviar cadastro</button> 
+
+       
+
       </form>
     </section>
   );
