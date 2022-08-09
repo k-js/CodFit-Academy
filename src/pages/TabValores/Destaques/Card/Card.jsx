8b9ca0f7f2d2d86868bfc81dc99ec54b14e64464
HellenCSilva <hellencosta2012@gmail.com>
2022-08-09 15:16:39 -0300

    Cards Prontos

--- src/pages/TabValores/Destaques/Card/Card.jsx
+++ src/pages/TabValores/Destaques/Card/Card.jsx
@@ -3,22 +3,20 @@ import { Link } from 'react-router-dom'
 
 export default function Card(props) {
   return (
-    <div className="flip-card">
+    <div className={S.flipCard}>
+      <h2 className={S.nomeCard}>{props.nome}</h2>
 
-        <div className="flip-card-front">
+        <div className={S.frente}>
 
           <img src={props.imagem} alt="Banner"/>
-
-        </div>
-
-        <div className="flip-card-back">
-
+          <p>
           <p>{props.p1}</p>
           <p>{props.p2}</p>
+          </p>
+
           <Link className={S.link} to={props.url}>{props.textoBotao}</Link>
 
-        </div>
-
+      </div>
       </div>
 
   );
