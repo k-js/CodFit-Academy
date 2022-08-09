b717f67ed5e6d829a38c5543fc52d0bdf957fbd4
HellenCSilva <hellencosta2012@gmail.com>
2022-08-09 16:10:51 -0300

    ajustes finais

--- src/pages/TabValores/Destaques/Destaques.jsx
+++ src/pages/TabValores/Destaques/Destaques.jsx
@@ -9,32 +9,33 @@ export default function Destaques() {
   return (
     <section className={S.destaques__container}>
 
-<h1>NOSSOS PLANOS</h1>
 
-      <aside className={S.topo}>
+      <section className={S.topo}>
 
       <div className={S.bloco1}>
 
-      <h1> FOCO, FORÇA e FAÇA SUA MATRICULA</h1>
-      <span> Valores incríveis, venha nos conhecer e escolha um de nossos planos!</span>
+      <h2 className={S.foco}> FOCO, FORÇA e FAÇA SUA MATRICULA</h2>
+      <span className={S.focomiudo}> Valores incríveis, venha nos conhecer e escolha um de nossos planos!</span>
 
       </div>
 
       <div className={S.bloco2}>
       
-      <h1><strong>50% de desconto</strong></h1>
-      <span>3 meses, apenas para os 50 primeiros matriculados em AGOSTO/SETEMBRO de 2022</span>
+      <h2 className={S.desconto}><strong>50% de desconto*</strong></h2>
+      <span className={S.miudo}>* 3 meses, apenas para os 50 primeiros matriculados em AGOSTO/SETEMBRO de 2022</span>
 
       </div>
 
-      </aside>
+      </section>
+     
+      {/* <h1 className={S.tituloplanos}>NOSSOS PLANOS</h1> */}
 
       <aside className={S.titulocontainer}>
         
         <Card
           nome="Mensal"
           imagem={Mensal}
-          p1="Apenas R$ 59,99 (sem fidelidade)"
+          p1="Apenas R$ 59,99"
           p2="50% Off de R$ 59,99 por R$ 29,99 por 3 meses"
           url="/CadClientes"
           textoBotao="Matricule-se"
@@ -57,8 +58,8 @@ export default function Destaques() {
         <Card 
           nome="Anual"
           imagem={Anual}
-          p1="Apenas R$ 599,99 ou 12x de R$49,99 (com fidelidade)"
-          p2="50% Off de R$ 599,99 por R$ 299,99 (não válido para parcelados)"
+          p1="Apenas R$ 599,99 ou 12x de R$49,99"
+          p2="50% Off de R$ 599,99 por R$ 299,99"
           url="/CadClientes"
           textoBotao="Matricule-se"
         />
