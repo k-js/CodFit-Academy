99b7952091d11446377623a998aceb1ce1af756c
Magali Lima <99837700+MagaliLimaDev@users.noreply.github.com>
2022-08-09 16:51:22 -0300

    ajustando código
    ajustando código

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,14 @@ import "./Footer.css";
 
 function Footer() {
     return (
-        <p>oii</p>
+        <p>COD ACADEMIA
+
+        Av. Anita Garibaldi, 300 | Cabral
+        
+        (ao lado do Resilia)
+        Tel.: (41) 3538-0000
+        
+        contato@codfitcademia.com.br</p>
     )
 }
 
